fb4d460c24441fa15b49d8a51c1093a349154731
dthewebd <gunkelmann.david@outlook.de>
2021-10-12 19:46:59 +0200

    added interface for axios

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,23 @@
 import { useState, useEffect } from "react";
-import axios, { AxiosResponse } from "axios";
+import axios from "axios";
 import { convertString } from "./services/convert";
 import { fisherYatesShuffle } from "./services/shuffle";
 import "./App.css";
 
-interface Question {
-  results: any;
+interface Fetch {
+  data?: {
+    response_code: number;
+    results: [
+      {
+        category: string;
+        type: string;
+        difficulty: string;
+        question: string;
+        correct_answer: string;
+        incorrect_answers: [];
+      }
+    ];
+  };
 }
 
 function App() {
@@ -17,22 +29,24 @@ function App() {
 
   useEffect(() => {
     const fetchQuestion = async () => {
-      const result: AxiosResponse<Question> = await axios.get(
+      const result: Fetch = await axios.get(
         "https://opentdb.com/api.php?amount=1&difficulty=easy&type=multiple"
       );
-      const question: string = result.data.results[0].question;
 
-      setQuestion(convertString(question));
+      if (result.data) {
+        const question: string = result.data.results[0].question;
+        setQuestion(convertString(question));
 
-      const correctAnswer: string = result.data.results[0].correct_answer;
-      const wrongAnswers: [] = result.data.results[0].incorrect_answers;
+        const correctAnswer: string = result.data.results[0].correct_answer;
+        const wrongAnswers: [] = result.data.results[0].incorrect_answers;
 
-      let multipleChoice = [...wrongAnswers, correctAnswer];
+        let multipleChoice = [...wrongAnswers, correctAnswer];
 
-      fisherYatesShuffle(multipleChoice);
-      setMultipleChoice(multipleChoice);
-      setCorrectAnswer(correctAnswer);
-      console.log(correctAnswer);
+        fisherYatesShuffle(multipleChoice);
+        setMultipleChoice(multipleChoice);
+        setCorrectAnswer(correctAnswer);
+        console.log(correctAnswer);
+      }
     };
 
     fetchQuestion();
